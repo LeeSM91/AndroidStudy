d614156d13f70c8f99865b8c847c71bbc39fba3e
LAPTOP-AKJDI3OE\smlee <E>
2022-05-18 15:57:10 +0900

    143p

--- Study1_06/app/src/main/java/com/example/sutdy1/MainActivity.kt
+++ Study1_06/app/src/main/java/com/example/sutdy1/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.sutdy1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val name = TextView(this).apply {
+            typeface = Typeface.DEFAULT_BOLD
+            text = "Lake Louise"
+        }
+
+        val image = ImageView(this).also {
+            it.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.menu1))
+        }
+
+        val address = TextView(this).apply {
+            typeface = Typeface.DEFAULT_BOLD
+            text = "Lake Louise, AB, 캐나다"
+        }
+
+
+
+        val layout = LinearLayout(this).apply {
+            orientation = LinearLayout.VERTICAL
+            gravity = Gravity.CENTER
+            addView(name, WRAP_CONTENT,WRAP_CONTENT)
+            addView(image, WRAP_CONTENT,WRAP_CONTENT)
+            addView(address, WRAP_CONTENT,WRAP_CONTENT)
+        }
+
+
+
+        setContentView(layout)
     }
+
+
+
 }
